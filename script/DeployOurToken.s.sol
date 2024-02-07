6674b0b9fb7c08b8e419b8a1f508ab3776b16046
marindimitrov12 <marin200041@gmail.com>
2024-02-07 16:56:01 +0200

    Adding test to the project

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.18;
 
 import{Script}from "forge-std/Script.sol";
 import{OurToken}from "../src/OurToken.sol";
+import{console}from"forge-std/console.sol";
 
 contract DeployOurToken is Script{
     uint256 public constant INITIAL_SUPPLY=1000 ether;
-    function run()external{
+    function run()external returns(OurToken){
     vm.startBroadcast();
-    new OurToken(INITIAL_SUPPLY);
+    console.log(msg.sender);
+    OurToken ot=new OurToken(INITIAL_SUPPLY);
     vm.stopBroadcast();
+    return ot;
     }
 }
